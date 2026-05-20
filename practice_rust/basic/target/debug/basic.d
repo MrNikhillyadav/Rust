5bf3e7c5cfd8cc34c8c88157e74709c285f6f057
Nikhil Y <nikyadav20032003@gmail.com>
2026-05-20 19:22:42 +0530

    add practice dir

---
+++ practice_rust/basic/target/debug/basic.d
@@ -0,0 +1,1 @@
+C:\Users\Nikhil\ Yadav\Rust\practice_rust\basic\target\debug\basic.exe: C:\Users\Nikhil\ Yadav\Rust\practice_rust\basic\src\main.rs
---
+++ practice_rust/basic/target/debug/deps/basic.d
@@ -0,0 +1,5 @@
+C:\Users\Nikhil Yadav\Rust\practice_rust\basic\target\debug\deps\basic.d: src\main.rs
+
+C:\Users\Nikhil Yadav\Rust\practice_rust\basic\target\debug\deps\basic.exe: src\main.rs
+
+src\main.rs:
